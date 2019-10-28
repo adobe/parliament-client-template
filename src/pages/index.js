6276b00826916b1c322864da0438c6fef50fdcc4
Simon MacDonald <simon.macdonald@gmail.com>
2019-10-28 08:59:09 -0400

    ✨  Index page generated from nav.json

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,31 +5,49 @@ import Heading from "@react/react-spectrum/Heading"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Heading variant="pageTitle">Adobe Developer Docs</Heading>
-    <p>
-      docs.adobe.io is the home for Adobe documentation for internal developers.
-      <br />
-      Check out our quickstarts, tutorials, API reference, and code examples.
-    </p>
-    <p>Now go build something great.</p>
-    <p>
-      <Heading variant="subtitle1">Docs Directory</Heading>
-    </p>
-    <ul>
-      <li>
-        <Link to="openapi/">Open API Remote</Link>
-      </li>
-      <li>
-        <Link to="README.md">Markdown git.corp repo</Link>
-      </li>
-      <li>
-        <Link to="hypermedia/">Hypermedia Example</Link>
-      </li>
-    </ul>
-  </Layout>
+const Item = ({ name, url, children }) => (
+  <li>
+    {!children ? <Link to={url}>{name}</Link> : name}
+    {children}
+  </li>
 )
 
+const nav = data => {
+  const subnav = children => {
+    if (children) {
+      return <ul>{nav(children)}</ul>
+    }
+  }
+
+  return data.map((node, index) => {
+    console.log(node)
+    return (
+      <Item key={node.id} name={node.label} url={node.url}>
+        {subnav(node.children)}
+      </Item>
+    )
+  })
+}
+
+const IndexPage = ({ data }) => {
+  const siteInfo = data.rawGatsbySourceGitJson
+  const pages = siteInfo.pages
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Heading variant="pageTitle">{siteInfo.title}</Heading>
+      {nav(pages)}
+    </Layout>
+  )
+}
+
 export default IndexPage
+
+export const query = graphql`
+  {
+    rawGatsbySourceGitJson {
+      title
+      pages
+    }
+  }
+`

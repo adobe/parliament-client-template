98bed9eaa4077232acb5acddd087a33ee4ab1d59
Simon MacDonald <simon.macdonald@gmail.com>
2020-01-27 23:23:02 -0500

    🐛  Fix footer finally

--- src/templates/markdownTemplate.js
+++ src/templates/markdownTemplate.js
@@ -15,21 +15,6 @@ const MarkdownTemplate = props => {
 
   const gitRemote = props.pageContext.gitRemote
 
-  /*
-.parent {
-display: grid;
-grid-template-columns: repeat(3, 1fr);
-grid-template-rows: repeat(2, 1fr);
-grid-column-gap: 0px;
-grid-row-gap: 0px;
-}
-
-.div1 { grid-area: 1 / 1 / 3 / 2; }
-.div2 { grid-area: 1 / 2 / 2 / 3; }
-.div3 { grid-area: 1 / 3 / 2 / 4; }
-.div4 { grid-area: 2 / 2 / 3 / 4; }
-*/
-
   return (
     <DocLayout>
       <SEO title={props.pageContext.seo} />
@@ -44,7 +29,7 @@ grid-row-gap: 0px;
       >
         <div
           css={css`
-            grid-area: 1 / 1 / 2 / 2;
+            grid-area: 1 / 1 / 3 / 2;
           `}
           className="spectrum--light"
         >
@@ -110,13 +95,13 @@ grid-row-gap: 0px;
             </span>
           </p>
         </div>
-      </div>
-      <div
-        css={css`
-          grid-area: 2 / 3 / 3 / 13;
-        `}
-      >
-        <Footer />
+        <div
+          css={css`
+            grid-area: 2 / 3 / 3 / 13;
+          `}
+        >
+          <Footer />
+        </div>
       </div>
     </DocLayout>
   )

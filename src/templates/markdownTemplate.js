0ece3b304157e2f4055df213cc5d57048721da1c
Simon MacDonald <simon.macdonald@gmail.com>
2020-01-30 19:25:18 -0500

    🔨  Use media queries instead of react-responsive

--- src/templates/markdownTemplate.js
+++ src/templates/markdownTemplate.js
@@ -3,8 +3,7 @@ import { css, jsx } from "@emotion/core"
 import { graphql } from "gatsby"
 import { useMediaQuery } from "react-responsive"
 import DocLayout from "../components/doclayout"
-import MarkdownDesktop from "../components/MarkdownDesktop"
-import MarkdownTablet from "../components/MarkdownTablet"
+import { Footer } from "@parliament/parliament-ui-components"
 import PageActions from "../components/PageActions"
 import SiteNav from "../components/SiteNav"
 import SEO from "../components/seo"
@@ -17,47 +16,106 @@ const MarkdownTemplate = props => {
 
   const gitRemote = props.pageContext.gitRemote
 
-  const isDesktop = useMediaQuery({ minWidth: 1201 })
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1200 })
-  const isMobile = useMediaQuery({ maxWidth: 767 })
+  return (
+    <DocLayout>
+      <SEO title={props.pageContext.seo} />
+      <div
+        css={css`
+        display: grid;
+        grid-template-rows: 1fr 30px
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+        @media screen and (min-width: 1201px) {
+          grid-template-columns: minmax(280px, 280px) repeat(11, 1fr);
+        }
+        @media screen and (min-width: 768px) {
+          grid-template-columns: minmax(280px, 280px) repeat(9, 1fr);
+        }
+      `}
+      >
+        <div
+          css={css`
+            grid-area: 1 / 1 / 3 / 2;
+          `}
+          className="spectrum--light"
+        >
+          <SiteNav
+            currentPage={props.location.pathname}
+            gitRemote={gitRemote}
+          />
+        </div>
+        <div
+          css={css`
+            padding-top: 30px;
+            padding-left: 16px;
+            padding-right: 16px;
 
-  const siteNav = (
-    <SiteNav currentPage={props.location.pathname} gitRemote={gitRemote} />
-  )
-  const content = renderAst(htmlAst)
-  const pageActions = (
-    <PageActions
-      gitRemote={gitRemote}
-      modifiedTime={modifiedTime}
-      relativePath={relativePath}
-      tableOfContents={tableOfContents}
-      timeToRead={timeToRead}
-    />
-  )
+            @media screen and (min-width: 1201px) {
+              grid-area: 1 / 2 / 2 / 11;
+            }
+            @media screen and (min-width: 768px) {
+              grid-area: 1 / 2 / 2 / 11;
+            }
+          `}
+        >
+          <div
+            css={css`
+              @media screen and (min-width: 1201px) {
+                display: none;
+              }
+              @media screen and (min-width: 768px) {
+                float: right;
+              }
+            `}
+          >
+            <PageActions
+              gitRemote={gitRemote}
+              modifiedTime={modifiedTime}
+              relativePath={relativePath}
+              tableOfContents={tableOfContents}
+              timeToRead={timeToRead}
+            />
+          </div>
 
-  if (isDesktop) {
-    return (
-      <DocLayout>
-        <SEO title={props.pageContext.seo} />
-        <MarkdownDesktop
-          siteNav={siteNav}
-          content={content}
-          pageActions={pageActions}
-        />
-      </DocLayout>
-    )
-  } else {
-    return (
-      <DocLayout>
-        <SEO title={props.pageContext.seo} />
-        <MarkdownTablet
-          siteNav={siteNav}
-          content={content}
-          pageActions={pageActions}
-        />
-      </DocLayout>
-    )
-  }
+          {renderAst(htmlAst)}
+        </div>
+        <div
+          css={css`
+            padding-top: 30px;
+            padding-left: 16px;
+            padding-right: 16px;
+
+            @media screen and (min-width: 1201px) {
+              grid-area: 1 / 11 / 2 / 13;
+            }
+            @media screen and (min-width: 768px) {
+              display: none;
+            }
+          `}
+        >
+          <PageActions
+            gitRemote={gitRemote}
+            modifiedTime={modifiedTime}
+            relativePath={relativePath}
+            tableOfContents={tableOfContents}
+            timeToRead={timeToRead}
+          />
+        </div>
+        <div
+          css={css`
+            @media screen and (min-width: 1201px) {
+              grid-area: 2 / 3 / 3 / 13;
+            }
+            @media screen and (min-width: 768px) {
+              grid-area: 2 / 3 / 3 / 11;
+            }
+          `}
+        >
+          <Footer />
+        </div>
+      </div>
+    </DocLayout>
+  )
 }
 
 export default MarkdownTemplate

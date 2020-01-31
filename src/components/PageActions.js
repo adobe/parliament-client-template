b144a18dc93376e2905c3a10677a6a78d40d8c2c
Simon MacDonald <simon.macdonald@gmail.com>
2020-01-30 19:45:01 -0500

    🖼️  Use css query in PageActions

--- src/components/PageActions.js
+++ src/components/PageActions.js
@@ -3,7 +3,6 @@ import React from "react"
 import { css, jsx } from "@emotion/core"
 import { Feedback } from "@parliament/parliament-ui-components"
 import Heading from "@react/react-spectrum/Heading"
-import { useMediaQuery } from "react-responsive"
 
 const PageActions = ({
   gitRemote,
@@ -12,7 +11,6 @@ const PageActions = ({
   modifiedTime,
   timeToRead,
 }) => {
-  const isDesktop = useMediaQuery({ minWidth: 1201 })
   return (
     <React.Fragment>
       <div
@@ -30,35 +28,40 @@ const PageActions = ({
           ""
         )}
       </div>
-      {isDesktop ? (
-        <React.Fragment>
-          <div>
-            <Heading variant="subtitle3">On this page</Heading>
-            <span
-              className="toc"
-              dangerouslySetInnerHTML={{ __html: tableOfContents }}
-            ></span>
-          </div>
-          <p>
-            <span
-              css={css`
-                display: block;
-              `}
-            >
-              Last update: {modifiedTime}
-            </span>
-            <span
-              css={css`
-                display: block;
-              `}
-            >
-              {timeToRead} min read
-            </span>
-          </p>
-        </React.Fragment>
-      ) : (
-        ""
-      )}
+      <div
+        css={css`
+          @media screen and (min-width: 1201px) {
+            display: block;
+          }
+          @media screen and (min-width: 768px) and (max-width: 1200px) {
+            display: none;
+          }
+        `}
+      >
+        <div>
+          <Heading variant="subtitle3">On this page</Heading>
+          <span
+            className="toc"
+            dangerouslySetInnerHTML={{ __html: tableOfContents }}
+          ></span>
+        </div>
+        <p>
+          <span
+            css={css`
+              display: block;
+            `}
+          >
+            Last update: {modifiedTime}
+          </span>
+          <span
+            css={css`
+              display: block;
+            `}
+          >
+            {timeToRead} min read
+          </span>
+        </p>
+      </div>
     </React.Fragment>
   )
 }

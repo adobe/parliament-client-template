ccfddffca5251843e529d143718109d32b429f20
Hiren Shah <hirenanandshah@outlook.com>
2020-02-06 13:41:12 -0700

    🗒️ changing prism theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,5 +5,5 @@
  */
 
 // You can delete this file if you're not using it
-require("prism-themes/themes/prism-ghcolors.css")
+require("prismjs/themes/prism-coy.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")

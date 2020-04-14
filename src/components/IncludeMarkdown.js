27576c60697d21e09ab9a37b9fd508aa0734772a
Simon MacDonald <simon.macdonald@gmail.com>
2020-04-14 15:44:14 -0400

    🐛  DEVEP-1927 - AFM: underscores in filenames of include_relative

--- src/components/IncludeMarkdown.js
+++ src/components/IncludeMarkdown.js
@@ -19,12 +19,16 @@ const IncludeMarkdown = ({ file }) => {
   )
 
   const markdownRemark = allMarkdownRemark.edges.find(edge =>
-    edge.node.fileAbsolutePath.endsWith(file)
+    edge.node.fileAbsolutePath.toLowerCase().endsWith(file.toLowerCase())
   )
 
-  return (
-    <React.Fragment>{renderAst(markdownRemark.node.htmlAst)}</React.Fragment>
-  )
+  if (markdownRemark) {
+    return (
+      <React.Fragment>{renderAst(markdownRemark.node.htmlAst)}</React.Fragment>
+    )
+  } else {
+    return <React.Fragment>Could not find {file}</React.Fragment>
+  }
 }
 
 export default IncludeMarkdown

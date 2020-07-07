4af9bc63c95c317af8b696b5a5b923504f94aee9
Simon MacDonald <simon.macdonald@gmail.com>
2020-07-07 11:16:12 -0400

    📝  Add license headers

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,19 @@
+/**
+ *  Copyright 2020 Adobe. All rights reserved.
+ *  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. You may obtain a copy
+ *  of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *  OF ANY KIND, either express or implied. See the License for the specific language
+ *  governing permissions and limitations under the License.
+ */
+
 module.exports = {
   transform: {
     "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
-    ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css"
+    ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css",
   },
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
@@ -15,5 +27,4 @@ module.exports = {
   testURL: `http://localhost`,
   setupFiles: [`<rootDir>/loadershim.js`],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-
 }

36a53d8cd6a60caa9c88151c82898a63d6fbc03b
James Calcaben <calcaben@adobe.com>
2020-07-21 15:33:11 -0500

    Refactor and add test for instances where pages is not defined

--- plugins/navigation-files-transformer/src/__tests__/fromJson.test.js
+++ plugins/navigation-files-transformer/src/__tests__/fromJson.test.js
@@ -42,6 +42,45 @@ test("manifest-docs.json content", () => {
   expect(parsedContent).toMatchSnapshot()
 })
 
+test("missing pages property", () => {
+  const fileContent = `
+{
+  "name": "Parliament Site",
+  "version": "1.0.0",
+  "description": "Onboarding docs for Parliament",
+  "author": "DevRel Team",
+  "view_type": "mdbook",
+  "meta_keywords": "adobe, parliament",
+  "meta_description": "default description",
+  "publish_date": "30/08/2018",
+  "show_edit_github_banner": false,
+  "base_path": "https://raw.githubusercontent.com",
+  "pages": [
+    {
+      "importedFileName": "readme",
+      "pages": [
+        {
+          "importedFileName": "onboarding",
+          "path": "DevRel/parliament-docs/master/onboarding.md",
+          "title": "Onboarding"
+        }
+      ],
+      "path": "DevRel/parliament-docs/master/README.md",
+      "title": "Overview"
+    },
+    {
+      "importedFileName": "contributing",
+      "path": "DevRel/parliament-docs/master/.github/CONTRIBUTING.md",
+      "title": "Contributing"
+    }
+  ]
+}
+`
+  const parsedContent = fromJson(fileContent)
+
+  expect(parsedContent).toMatchSnapshot()
+})
+
 test("Valid json but not a navigation file", () => {
   const fileContent = `
 {

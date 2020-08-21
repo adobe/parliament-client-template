91388e519c9914a0aefa39d49ae5968ad2049b1c
James Calcaben <calcaben@adobe.com>
2020-08-21 17:18:10 -0500

    Clean up plugin entry file and add JSDocs

--- plugins/swagger-to-graphql-source/gatsby-node.js
+++ plugins/swagger-to-graphql-source/gatsby-node.js
@@ -1,79 +1,111 @@
-const path = require('path')
+const path = require("path")
 const glob = require("fast-glob")
-const SwaggerParser = require("@apidevtools/swagger-parser");
+const SwaggerParser = require("@apidevtools/swagger-parser")
 
-const { createPathNodes, createDefinitionNodes } = require('./src/swaggerGraphQLNodesFactories');
-const createInfoNode = require('./src/createInfoNode');
+const createInfoNode = require("./src/createInfoNode")
+const createPathNodes = require("./src/createPathNodes")
+const createDefinitionNodes = require("./src/createDefinitionNodes")
 
-exports.sourceNodes = async ({
+/**
+ * Taps into Gatsby's `sourceNodes` lifecycle to create nodes from a source.
+ *
+ * This plugin creates GraphQL nodes from swagger/openapi files in a project.
+ *
+ * @param {Object} nodeApiHelpers [Node API Helpers]{@link https://www.gatsbyjs.com/docs/node-api-helpers/}
+ * @param {Object} pluginOptions Options for this plugin
+ * @param {String} pluginOptions.contentRoot The root directory to begin the search for swagger/openapi files.
+ * This files must be available as a Gatsby file source through a plugin such as `gatsby-source-filesystem`.
+ * @param {String} pluginOptions.sourcePatterns A comma-separated string of glob patterns used for searching for swagger/openapi files.
+ *
+ * @see {@link https://www.gatsbyjs.com/docs/creating-a-source-plugin/}
+ * @see {@link https://www.gatsbyjs.com/docs/node-apis/#sourceNodes}
+ */
+exports.sourceNodes = async (nodeApiHelpers, pluginOptions) => {
+  const {
     actions,
     createContentDigest,
     createNodeId,
     getNodesByType,
-}, pluginOptions) => {
+  } = nodeApiHelpers
 
-    const { createNode, createParentChildLink } = actions
+  const fileNodes = getNodesByType("File")
 
-    const fileNodes = getNodesByType('File');
+  const { createNode, createParentChildLink } = actions
 
-    const { contentRoot, sourcePatterns } = pluginOptions;
+  const gatsbyApi = {
+    createNodeId,
+    createContentDigest,
+  }
 
-    const patternsArray = sourcePatterns.split(",")
+  const { contentRoot, sourcePatterns } = pluginOptions
 
-    const globOptions = {
-        cwd: contentRoot,
-        nodir: true,
-    }
+  const globOptions = {
+    cwd: contentRoot,
+    nodir: true,
+  }
 
-    const fileResults = glob.sync(patternsArray, globOptions)
+  const patternsArray = sourcePatterns.split(",")
 
-    fileResults.forEach(file => {
-        const fileInfo = path.parse(file)
+  const swaggerFiles = glob.sync(patternsArray, globOptions)
 
-        const { dir, base, name } = fileInfo
+  swaggerFiles.forEach(parentFile => {
+    const parentFileInfo = path.parse(parentFile)
 
-        const filePath = path.resolve(contentRoot, dir, base);
+    const { dir, base } = parentFileInfo
 
-        const fileNode = fileNodes.find(fileNode => fileNode.absolutePath === filePath);
+    const parentFilePath = path.resolve(contentRoot, dir, base)
 
-        const gatsbyNodeApi = {
-            createNodeId,
-            createContentDigest,
-        }
+    const parentFileNode = fileNodes.find(
+      fileNode => fileNode.absolutePath === parentFilePath
+    )
 
-        SwaggerParser.parse(filePath)
-            .then(api => {
-                console.log(filePath);
+    SwaggerParser.parse(parentFilePath).then(swaggerObject => {
+      const infoNode = createInfoNode({
+        swaggerObject,
+        parentFile: parentFileNode,
+        gatsbyApi,
+      })
+      const pathNodes = createPathNodes({
+        swaggerObject,
+        parentFile: parentFileNode,
+        gatsbyApi,
+      })
+      const definitionNodes = createDefinitionNodes({
+        swaggerObject,
+        parentFile: parentFileNode,
+        gatsbyApi,
+      })
 
-                const infoNode = createInfoNode({ api, parentFile: fileNode, gatsbyNodeApi })
-                const pathNodes = createPathNodes({api, parentFile: fileNode, gatsbyNodeApi })
-                const definitionNodes = createDefinitionNodes({api, parentFile: fileNode, gatsbyNodeApi });
+      const newNodes = [infoNode, ...pathNodes, ...definitionNodes]
 
-                const newNodes = [
-                    infoNode,
-                    ...pathNodes,
-                    ...definitionNodes 
-                ]
-
-                newNodes.forEach(pathNode => {
-                    createNode(pathNode);
-                    createParentChildLink({ parent: fileNode, child: pathNode})
-                })
-            })
-
-    });
-
+      newNodes.forEach(newNode => {
+        createNode(newNode)
+        createParentChildLink({ parent: parentFileNode, child: newNode })
+      })
+    })
+  })
 }
 
-// PoC that you can roll up data into a single JSON object
-// See: https://www.gatsbyjs.com/docs/schema-customization/
-exports.createSchemaCustomization = ({ actions }) => {
-    const { createTypes } = actions
-    const typeDefs = `
-    type SwaggerOpenApiInfo implements Node {
-      license: JSON
-    }
+/**
+ * Taps into the Gatsby API for customizing the generated schema.
+ *
+ * This plugin tells Gatsby to interpret specific properties as JSON objects.
+ * The value for these properties do not follow a specific
+ * object shape, so it's best to interpret them as JSON objects
+ * to make querying the data easier.
+ *
+ * @param {Object} nodeApiHelpers [Node API Helpers]{@link https://www.gatsbyjs.com/docs/node-api-helpers/}
+ *
+ * @see {@link https://www.gatsbyjs.com/docs/node-apis/#createSchemaCustomization}
+ * @see {@link https://www.gatsbyjs.com/docs/schema-customization/}
+ * @see {@link https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#schemaObject}
+ */
+exports.createSchemaCustomization = nodeApiHelpers => {
+  const { actions } = nodeApiHelpers
 
+  const { createTypes } = actions
+
+  const typeDefs = `
     type SwaggerOpenApiPathOperations {
         parameters: JSON
         responses: JSON
@@ -83,5 +115,5 @@ exports.createSchemaCustomization = ({ actions }) => {
         schema: JSON
     }
   `
-    createTypes(typeDefs)
+  createTypes(typeDefs)
 }

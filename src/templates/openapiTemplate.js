5bbd68d169ae41ede7ad200c71cc1417f6f0e93d
Simon MacDonald <simon.macdonald@gmail.com>
2020-08-25 10:44:42 -0400

    Add spectrum styled open api component (#18)
    * ⬆️  Use UI components 2.2.0
    
    * ✨  Use OpenAPIBlock
    
    * ➖  Remove ReDoc
    
    * ⬆️  Update gatsby-remark-afm

--- src/templates/openapiTemplate.js
+++ src/templates/openapiTemplate.js
@@ -12,12 +12,11 @@
 
 /** @jsx jsx */
 import { jsx } from "@emotion/core"
-import { RedocStandalone } from "redoc"
 
 import SiteNav from "../components/SiteNav"
 import DocLayout from "../components/doclayout"
 import SEO from "../components/seo"
-import { Footer } from "@adobe/parliament-ui-components"
+import { Footer, OpenAPIBlock } from "@adobe/parliament-ui-components"
 
 import {
   OpenApiGrid,
@@ -41,13 +40,7 @@ const OpenApiTemplate = ({ data, pageContext, location }) => {
           />
         </OpenApiGridNav>
         <OpenApiGridContent>
-          <RedocStandalone
-            spec={pageContext.spec}
-            options={{
-              hideLoading: true,
-              menuToggle: true,
-            }}
-          />
+          <OpenAPIBlock spec={pageContext.spec} />
         </OpenApiGridContent>
         <OpenApiGridFooter>
           <Footer />

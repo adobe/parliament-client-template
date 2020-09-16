7c64aed71ddfa2ee63774d011198bed316cfa8bc
Simon MacDonald <simon.macdonald@gmail.com>
2020-09-16 17:37:55 -0400

    📝  Fix header comment

--- src/components/componentsMapping.js
+++ src/components/componentsMapping.js
@@ -1,3 +1,15 @@
+/**
+ *  Copyright 2020 Adobe. All rights reserved.
+ *  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. You may obtain a copy
+ *  of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *  OF ANY KIND, either express or implied. See the License for the specific language
+ *  governing permissions and limitations under the License.
+ */
+
 import {
   Alert,
   InlineCode,
@@ -27,8 +39,6 @@ export const componentsMapping = {
   h4: Heading4,
   h5: Heading5,
   h6: Heading6,
-  // tab: Tab,
-  // tabview: TabView,
   table: Table,
   tbody: TBody,
   th: Th,
--- src/components/doclayout.js
+++ src/components/doclayout.js
@@ -10,13 +10,6 @@
  *  governing permissions and limitations under the License.
  */
 
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 

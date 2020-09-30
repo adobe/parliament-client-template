ff0c0863480cb95741ae4bb72edfd83e5edee4a5
Simon MacDonald <simon.macdonald@gmail.com>
2020-09-29 21:28:21 -0400

    🖼️  Use Header from ui components

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -11,124 +11,24 @@
  */
 
 /** @jsx jsx */
-import { css, jsx } from "@emotion/core"
-import { useEffect, useRef, useState } from "react"
-import { Link } from "gatsby"
-import Logo from "../images/adobe_logo-2.svg"
-import { ActionButton, Divider, Flex, View } from "@adobe/react-spectrum"
+import { jsx } from "@emotion/core"
+import { Header } from "@adobe/parliament-ui-components"
 import SiteMenu from "./SiteMenu"
-
-import Menu from "@spectrum-icons/workflow/ShowMenu"
 
 import { useMediaQuery } from "react-responsive"
 
-const stretched = css`
-  height: 100%;
-`
-
 const HeaderBar = ({ siteTitle, forceMobile, ...props }) => {
   const isMobile = useMediaQuery({ maxWidth: 767 })
-  let [isPopoverOpen, setPopoverOpen] = useState(false)
-  const node = useRef()
-
-  const handleClickOutside = e => {
-    if (node.current.contains(e.target)) {
-      return
-    }
-    setPopoverOpen(false)
-  }
-
-  useEffect(() => {
-    if (isPopoverOpen) {
-      document.addEventListener("mousedown", handleClickOutside)
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [isPopoverOpen])
 
   return (
-    <header
-      role="banner"
-      css={css`
-        ${stretched}
-        border-bottom: var(--spectrum-global-dimension-static-size-10) solid var(--spectrum-global-color-gray-200);
-        box-sizing: border-box;
-      `}
-    >
-      <nav css={stretched} role="navigation" aria-label="Global">
-        <View height="100%" paddingStart="size-300" paddingEnd="size-300">
-          <Flex alignItems="center" height="100%">
-            {isMobile || forceMobile ? (
-              <ActionButton
-                isQuiet
-                onPress={() => {
-                  setPopoverOpen(!isPopoverOpen)
-                }}
-              >
-                <Menu UNSAFE_style={{ paddingRight: 0, paddingLeft: 0 }} />
-              </ActionButton>
-            ) : (
-              undefined
-            )}
-            <a
-              href="https://developers.corp.adobe.com"
-              css={css`
-                text-decoration-line: none;
-              `}
-            >
-              <Flex alignItems="center" height="100%">
-                <Logo
-                  css={css`
-                    width: var(--spectrum-global-dimension-static-size-450);
-                    height: var(--spectrum-global-dimension-static-size-400);
-                    display: block;
-                    margin-right: var(
-                      --spectrum-global-dimension-static-size-200
-                    );
-                  `}
-                />
-                <strong className="spectrum-Heading--XXS">Developer</strong>
-              </Flex>
-            </a>
-            <Divider
-              orientation="vertical"
-              marginStart="size-300"
-              size="M"
-              height="100%"
-            />
-            <Link
-              css={css`
-                text-decoration-line: none;
-              `}
-              to="/"
-            >
-              <View paddingStart="size-300">
-                <span className="spectrum-Body spectrum-Body--XXS">
-                  {siteTitle}
-                </span>
-              </View>
-            </Link>
-          </Flex>
-        </View>
-      </nav>
-      {isMobile || forceMobile ? (
-        <div
-          className={`spectrum-Site-sideBar ${isPopoverOpen ? " is-open" : ""}`}
-          ref={node}
-          css={css`
-            margin-top: 64px;
-          `}
-        >
-          <SiteMenu isMobile={isMobile} {...props} />
-        </div>
-      ) : (
-        undefined
-      )}
-    </header>
+    <Header
+      title="Developer"
+      titleUrl="https://developers.corp.adobe.com"
+      siteTitle={siteTitle}
+      forceMobile={forceMobile}
+      menu={<SiteMenu isMobile={isMobile} {...props} />}
+      {...props}
+    />
   )
 }
 

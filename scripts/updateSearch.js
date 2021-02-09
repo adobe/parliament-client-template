8b9db8baf72a09b7691e508df472b72bcb203080
Simon MacDonald <simon.macdonald@gmail.com>
2021-02-09 11:46:35 -0500

    🐛  Private projects should not be searchable

--- scripts/updateSearch.js
+++ scripts/updateSearch.js
@@ -4,20 +4,22 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 ;(async () => {
-  const data = JSON.parse(fs.readFileSync("./searchIndex.json"))
+  if (process.env.VISIBLE && process.env.VISIBLE !== "false") {
+    const data = JSON.parse(fs.readFileSync("./searchIndex.json"))
 
-  const response = await fetch(
-    `${process.env.SEARCH_URL}/projects/${process.env.JOB_NAME}`,
-    {
-      method: "PATCH",
-      body: JSON.stringify({ id: `${process.env.JOB_NAME}`, data }),
-      headers: { 
-        "Content-Type": "application/json",
-        "x-api-key": `${process.env.X_API_KEY}`
-      },
-    }
-  )
-  const json = await response.json()
+    const response = await fetch(
+      `${process.env.SEARCH_URL}/projects/${process.env.JOB_NAME}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({ id: `${process.env.JOB_NAME}`, data }),
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": `${process.env.X_API_KEY}`,
+        },
+      }
+    )
+    const json = await response.json()
 
-  console.log(json)
+    console.log(json)
+  }
 })()

0397bfba7150ec1bf1db41f1aed8b959d44e20fa
Simon MacDonald <simon.macdonald@gmail.com>
2021-02-11 15:30:09 -0500

    🔍  If VISIBLE is not set send search index

--- scripts/updateSearch.js
+++ scripts/updateSearch.js
@@ -4,7 +4,7 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 ;(async () => {
-  if (process.env.VISIBLE && process.env.VISIBLE !== "false") {
+  if (!process.env.VISIBLE || process.env.VISIBLE !== "false") {
     const data = JSON.parse(fs.readFileSync("./searchIndex.json"))
 
     const response = await fetch(

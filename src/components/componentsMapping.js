0efc1ba34d32538c21c2132d6cfab49cab055fef
Simon MacDonald <simon.macdonald@gmail.com>
2021-04-06 13:35:47 -0400

    💄  Spectrum sizing tweaks

--- src/components/componentsMapping.js
+++ src/components/componentsMapping.js
@@ -9,7 +9,7 @@
  *  OF ANY KIND, either express or implied. See the License for the specific language
  *  governing permissions and limitations under the License.
  */
-
+import React from "react"
 import {
   Alert,
   InlineCode,
@@ -55,6 +55,11 @@ export const componentsMapping = {
   tr: Tr,
   p: Paragraph,
   ul: List,
+  ol: ({ children, ...props }) => (
+    <List elementType="ol" {...props}>
+      {children}
+    </List>
+  ),
   pre: Pre,
   inlineCode: InlineCode,
   code: InlineCode,

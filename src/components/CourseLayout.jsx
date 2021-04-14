cfe6b968a410ccc8bb9564f4f8ea53ba2fe56ef1
chr0n1x <heilong24@gmail.com>
2021-04-14 17:57:09 -0400

    [WIP 🛠 ] Course Template

---
+++ src/components/CourseLayout.jsx
@@ -0,0 +1,110 @@
+/**
+ *  Copyright 2020 Adobe. All rights reserved.
+ *  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. You may obtain a copy
+ *  of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *  OF ANY KIND, either express or implied. See the License for the specific language
+ *  governing permissions and limitations under the License.
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+
+import { graphql, useStaticQuery } from "gatsby"
+
+import { SSRProvider } from "@adobe/react-spectrum"
+import {
+  Footer,
+  Grid,
+  GridHeader,
+  GridNav,
+  GridContent,
+  GridFooter,
+  GridRightRail,
+  Provider,
+} from "@adobe/parliament-ui-components"
+
+import HeaderBar from "./HeaderBar"
+import SEO from "./seo"
+
+import "./layout.css"
+
+const CourseLayout = ({
+  children,
+  title,
+  location,
+  gitRemote,
+  currentPage,
+  pages,
+  forceMobile = false,
+  sideNav,
+  rightRail,
+}) => {
+  const { allSiteTabs, allHeaderTabs } = useStaticQuery(
+    graphql`
+      query {
+        allHeaderTabs {
+          edges {
+            node {
+              path
+              id
+              title
+            }
+          }
+        }
+        allSiteTabs {
+          edges {
+            node {
+              path
+              id
+              title
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const tabs = [
+    ...allSiteTabs.edges.map(({ node }) => node),
+    ...allHeaderTabs.edges.map(({ node }) => node),
+  ]
+
+  return (
+    <SSRProvider>
+      <SEO title={title} />
+      <Provider>
+        <Grid>
+          <GridHeader>
+            <HeaderBar
+              location={location}
+              gitRemote={gitRemote}
+              pages={pages}
+              forceMobile={forceMobile}
+              tabs={tabs}
+            />
+          </GridHeader>
+          {sideNav && <GridNav>{sideNav}</GridNav>}
+          {sideNav ? (
+            <GridContent>{children}</GridContent>
+          ) : (
+            <div style={{ gridArea: "2 / 1 / 3 / 14" }}>{children}</div>
+          )}
+          {rightRail && <GridRightRail>{rightRail}</GridRightRail>}
+          <GridFooter>
+            <Footer />
+          </GridFooter>
+        </Grid>
+      </Provider>
+    </SSRProvider>
+  )
+}
+
+CourseLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default CourseLayout
---
+++ src/templates/course.jsx
@@ -0,0 +1,123 @@
+/**
+ *  Copyright 2020 Adobe. All rights reserved.
+ *  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. You may obtain a copy
+ *  of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *  OF ANY KIND, either express or implied. See the License for the specific language
+ *  governing permissions and limitations under the License.
+ */
+
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react"
+import { graphql } from "gatsby"
+import CourseLayout from "../components/CourseLayout"
+import PageActions from "../components/PageActions"
+import SiteMenu from "../components/SiteMenu"
+import RenderMdx from "../components/RenderMdx"
+
+import { Flex, View } from "@adobe/react-spectrum"
+import {
+  ActionButtons,
+  Contributors,
+  Link,
+} from "@adobe/parliament-ui-components"
+
+const CourseTemplate = ({ data, location, pageContext }) => {
+  return (<p>`{JSON.stringify(data)}`</p>);
+
+  const { file, parliamentNavigation, site } = data;
+  const { siteMetadata } = site;
+  const { sourceFiles } = siteMetadata;
+  const { absolutePath, childMdx } = file
+  const { body, tableOfContents, timeToRead } = childMdx
+  const { contributors, gitRemote } = pageContext
+  const pathToFiles = sourceFiles.endsWith("/")
+    ? sourceFiles
+    : `${sourceFiles}/`
+  const relativePath = absolutePath.replace(pathToFiles, "")
+
+  return (
+    <CourseLayout
+      title={pageContext.seo}
+      location={location}
+      gitRemote={gitRemote}
+      currentPage={location.pathname}
+      pages={parliamentNavigation.pages}
+      sideNav={
+        <SiteMenu
+          currentPage={location.pathname}
+          gitRemote={gitRemote}
+          pages={parliamentNavigation.pages}
+        />
+      }
+      rightRail={
+        <aside
+          css={css`
+            position: fixed;
+            top: var(--spectrum-global-dimension-size-800);
+            height: 100%;
+          `}
+        >
+          <PageActions
+            gitRemote={gitRemote}
+            relativePath={relativePath}
+            tableOfContents={tableOfContents}
+            timeToRead={timeToRead}
+          />
+          Powered by{" "}
+          <Link href="https://developers.corp.adobe.com/parliament-docs/README.md">
+            Parliament
+          </Link>
+        </aside>
+      }
+    >
+      <div
+        css={css`
+          float: right;
+          z-index: 100;
+        `}
+      >
+        {gitRemote !== null ? (
+          <ActionButtons
+            gitUrl={`${gitRemote.protocol}://${gitRemote.resource}/${gitRemote.full_name}`}
+            filePath={relativePath}
+            branch={gitRemote.ref}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+      <RenderMdx>{body}</RenderMdx>
+
+      // TODO: inject link to next module here
+      <Link href="/courses/zero-to-production/02_onboard_to_orca.md">
+        --> Next Lesson Onboard your service to OrCA
+      </Link>
+
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        marginTop="size-800"
+        marginBottom="size-400"
+      >
+        <View>
+          <Contributors
+            href={`${gitRemote.protocol}://${gitRemote.resource}/${gitRemote.full_name}/blob/${gitRemote.ref}/${relativePath}`}
+            contributors={contributors}
+            date={
+              contributors[0]
+                ? new Date(contributors[0].date).toLocaleDateString()
+                : new Date().toLocaleDateString()
+            }
+          />
+        </View>
+      </Flex>
+    </CourseLayout>
+  )
+
+};
+
+export default CourseTemplate;

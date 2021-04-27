e7d22d3298f409dc75e314fc15efc073e2a116f7
Simon MacDonald <simon.macdonald@gmail.com>
2021-04-27 18:12:19 -0400

    🔧  Make eslint and prettier play nice

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    module: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "prettier",
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2021,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    // TODO: go through each of these, maybe autofix?
-    "rules": {
-      "react/prop-types": "off",
-      "react/jsx-key": "off",
-      "react/display-name": "off",
-    }
-};
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  // TODO: go through each of these, maybe autofix?
+  rules: {
+    "react/prop-types": "off",
+    "react/jsx-key": "off",
+    "react/display-name": "off",
+  },
+}

0f24240c80d24aa9bcbc4b46c1efb0bf81aa2764
Simon MacDonald <simon.macdonald@gmail.com>
2021-05-20 13:10:45 -0400

    useHooks

--- src/components/ProgressBarDrawer.js
+++ src/components/ProgressBarDrawer.js
@@ -1,4 +1,4 @@
-import { React, Component } from "react"
+import { React, useState } from "react"
 import Breakdown from "@spectrum-icons/workflow/Breakdown"
 import "@spectrum-css/typography"
 
@@ -6,52 +6,35 @@ import { Drawer } from "rsuite"
 
 import ProgressBar from "../components/ProgressBar"
 
-const mouseOverStyles = { textAlign: 'left' };
+const mouseOverStyles = { textAlign: "left" }
 const drawerStyles = {
-  backgroundColor: 'inherit',
-  colorScheme: 'inherit',
-};
+  backgroundColor: "inherit",
+  colorScheme: "inherit",
+}
 
-class ProgressBarDrawer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false
-    };
-    this.close = this.close.bind(this);
-    this.toggleDrawer = this.toggleDrawer.bind(this);
-  }
+const ProgressBarDrawer = ({ pages, currentIx }) => {
+  const [show, setShow] = useState(false)
+  return (
+    <div>
+      <span onMouseOver={() => setShow(true)} style={mouseOverStyles}>
+        <Breakdown /> Course Module List
+      </span>
+      <Drawer
+        show={show}
+        onHide={() => setShow(false)}
+        size={"xs"}
+        style={drawerStyles}
+      >
+        <Drawer.Header>
+          <Drawer.Title>Course Modules</Drawer.Title>
+        </Drawer.Header>
 
-  close() { this.setState({ show: false }); }
-  toggleDrawer() { this.setState({ show: true }); }
-
-  render() {
-    return (
-        <div>
-          <span onMouseOver={this.toggleDrawer} style={mouseOverStyles}>
-            <Breakdown />{" "}Course Module List
-          </span>
-          <Drawer
-            show={this.state.show}
-            onHide={this.close}
-            size={'xs'}
-            style={drawerStyles}
-          >
-
-            <Drawer.Header>
-              <Drawer.Title>Course Modules</Drawer.Title>
-            </Drawer.Header>
-
-            <Drawer.Body>
-              <ProgressBar
-                pages={this.props.pages}
-                currentIx={this.props.currentIx}
-              />
-            </Drawer.Body>
-          </Drawer>
-        </div>
-    );
-  }
+        <Drawer.Body>
+          <ProgressBar pages={pages} currentIx={currentIx} />
+        </Drawer.Body>
+      </Drawer>
+    </div>
+  )
 }
 
 export default ProgressBarDrawer

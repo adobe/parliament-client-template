48e08558dcedbb41dc1aeb5d4408ea8d8645a187
Hiren Shah <hirenanandshah@outlook.com>
2021-09-13 19:38:15 -0600

    🐛 refactoring

--- gatsby-node.js
+++ gatsby-node.js
@@ -629,7 +629,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       // Put main before module else it messes up react spectrum css import
-      mainFields: ["browser", "module", "main"],
+      mainFields: ["browser", "main", "module"],
     },
   })
 }

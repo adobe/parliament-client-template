770c1bc683dc2d5f965b71a104e6e1478ba46279
Hiren Anand Shah <hirenanandshah@outlook.com>
2021-10-15 12:41:56 -0600

    Revert "Revert "Course/Quiz specific nav""
    This reverts commit 3438f4c667b88feb266f95178704e1e3f2f3f187.

---
+++ src/components/CourseMenu.js
@@ -0,0 +1,62 @@
+
+/**
+ *  Copyright 2020 Adobe. All rights reserved.
+ *  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. You may obtain a copy
+ *  of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ *  OF ANY KIND, either express or implied. See the License for the specific language
+ *  governing permissions and limitations under the License.
+ */
+
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react"
+import PropTypes from "prop-types"
+
+import { CourseNav } from "../components/CourseNav"
+import "./sitenav.css"
+
+// NOTE: spectrum _visually_ renders everything past 2 levels the same
+// i.e.: it seemingly flattens all of the pages. Consequently, depth only
+// controls the _number_ of pages rendered, allowing templates to logically
+// organize/group their pages. It does not visually change anything.
+const CourseMenu = ({ currentPage, pages, completedModulePaths }) => {
+  return (
+    <div
+      css={css`
+        width: 256px;
+        margin: var(--spectrum-global-dimension-size-0);
+      `}
+    >
+      <div
+        css={css`
+          padding: var(--spectrum-global-dimension-size-0)
+            var(--spectrum-global-dimension-size-300);
+          height: 80vh;
+          overflow-y: auto;
+          overflow-x: hidden;
+        `}
+      >
+        <CourseNav
+          data={pages}
+          selected={currentPage}
+          completedModulePaths={completedModulePaths}
+        />
+      </div>
+    </div>
+  )
+}
+
+CourseMenu.propTypes = {
+  currentPage: PropTypes.string,
+  forceMobile: PropTypes.bool,
+}
+
+CourseMenu.defaultProps = {
+  currentPage: "",
+  forceMobile: false,
+}
+
+export default CourseMenu
---
+++ src/components/CourseNav.js
@@ -0,0 +1,84 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import '@spectrum-css/sidenav'
+
+const isPathSelected = (path, selected) => {
+    const compare = selected.endsWith('/')
+      ? selected.substring(0, selected.length - 1)
+      : selected
+    return path === compare
+}
+
+const liStyles = { paddingLeft: `calc(var(--spectrum-global-dimension-size-150))` }
+
+const navItemClass = (path, selectedPathUri, completedModulePaths) => {
+  let className = isPathSelected(path, selectedPathUri)
+                  ? 'spectrum-SideNav-item is-selected'
+                  : 'spectrum-SideNav-item'
+
+  if (completedModulePaths.includes(path) || selectedPathUri === path) {
+    return className
+  }
+
+  return `${className} is-disabled`
+}
+
+const navListItemLink = (liIndex, path, title, selected, completedModulePaths) => (
+  <li className={navItemClass(path, selected, completedModulePaths)} key={liIndex}>
+    <a
+      href={path}
+      className='spectrum-SideNav-itemLink'
+      style={liStyles}
+    >
+      {title}
+    </a>
+  </li>
+)
+
+const navListItemDeadLink = (liIndex, path, title, selected, completedModulePaths) =>
+  navListItemLink(liIndex, '#', title, selected, completedModulePaths)
+
+const nav = (data, selected, completedModulePaths) => {
+    return (
+      <ul className='spectrum-SideNav spectrum-SideNav--multiLevel'>
+        {data.map(
+          (node, index) => {
+            if (completedModulePaths.includes(node.path) || node.path == selected) {
+              return navListItemLink(index, node.path, node.title, selected, completedModulePaths)
+            }
+
+            return navListItemDeadLink(index, node.path, node.title, selected, completedModulePaths)
+          }
+        )}
+      </ul>
+    )
+}
+
+const CourseNav = ({ data = [], selected = '', ...props }) => {
+  const { completedModulePaths } = props
+  const defaultFocus = selected.charAt(0) === '/' ? selected : `/${selected}`
+  return (
+        <nav aria-label='Course Side Navigation' {...props}>
+          {nav(data, defaultFocus, completedModulePaths)}
+        </nav>
+      )
+}
+
+CourseNav.propTypes = {
+    data: PropTypes.array,
+    selected: PropTypes.string,
+    depth: PropTypes.number
+}
+
+export { CourseNav }
